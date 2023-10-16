f73b68610bcecc3d255c876d744f1953e314c80f
TFlynnFRCC <weegeeflower2002@gmail.com>
2023-10-16 17:52:58 -0600

    Does funky object related things

---
+++ src/Time.java
@@ -0,0 +1,26 @@
+
+public class Time {
+    private int hour;
+    private int minute;
+    private double second;
+    public Time() {
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0;
+    }
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public void increment(int seconds) {
+
+    }
+    public static void print(boolean b, Time) {
+        System.out.println(Time);
+    }
+    public Object twentyFour(String t3) {
+        String.split(":");
+        Interger.parseInt();
+    }
+}
